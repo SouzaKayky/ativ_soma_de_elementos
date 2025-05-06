8e8bfad8868d8b260dc3744e6a0f17d69d4807b3
kayky Souza <kaykysoares055@gmail.com>
2025-05-05 21:33:50 -0300

    ativ_soma_de_elementos

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main{ 
+    public static void main(String[] args) {
+        int[] vetor = { 
+            1,2,3,4,5,6,7,8,9,10
+        };
+        int soma = 0; 
+
+        for (int i = 0; i < vetor.length; i++){
+            soma += vetor[i]; 
+        }
+        System.out.println("A soma dos elementos do vetor é: " + soma); 
+
+    }   
+}
